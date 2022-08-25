d42a0fb81fb473a6d56c83dfd37c2a075dc07c46
natalliaamorim <natallia.nascimneto1@gmail.com>
2022-08-25 00:15:16 -0300

    teste frontendmog

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav-bar cor = "depp-purple lighten-1" logo ="Logo Estilizado" url="home"></nav-bar>
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/layouts/NavBar.vue';
+
+
+export default {
+  name: 'App' ,
+  components:{
+    NavBar,
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/layouts/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav :class = "cor ||'blue'">
+    <div class="nav-wrapper container">
+      <a :href="url ||'#'" class="brand-logo">{{logo || 'Site VueJs'}}</a>
+      
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: ['logo' , 'url' , 'cor'],
+  data () {
+    return {
+    //   msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
